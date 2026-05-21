cd93c143c8e4235da6a6cdc2f570a09e3ac61b6b
David Andersson <lejonmanen@gmail.com>
2026-05-21 11:10:08 +0200

    Subtract: refactor

--- src/math.js
+++ src/math.js
@@ -1,12 +1,15 @@
 
 
 function subtract(x, y) {
-	if( typeof x !== 'number' ) {
-		throw new Error('x must be a number')
-	} else if( typeof y !== 'number' ) {
-		throw new Error('y must be a number')
-	}
+	mustBeNumber(x, 'x must be a number')
+	mustBeNumber(y, 'y must be a number')
 	return x - y
+}
+
+function mustBeNumber(value, message) {
+	if( typeof value !== 'number' ) {
+		throw new Error(message)
+	}
 }
 
 export { subtract }
